b2157d89c3b6f578a4abacbedba5320d421001d2
nihaadk <nihad.keric@emineo.ch>
2024-02-12 07:43:40 +0100

    Init Application

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+
+const Layout = ({
+                    children,
+                }: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
---
+++ app/(dashboard)/tours/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToursPage = () => {
+    return (
+        <div>
+            Tours Page
+        </div>
+    );
+};
+
+export default ToursPage;
